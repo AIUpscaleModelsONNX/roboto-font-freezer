035ff1c8a6297026495ccce84158e2baa1a8363c
AIUpscaleModelsONNX <justisburnett02@gmail.com>
2026-03-03 17:03:48 -0500

    Create freeze-font.py to freeze OpenType features in fonts

---
+++ freeze-font.py
@@ -0,0 +1,14 @@
+# freeze-font.py
+
+"""
+This script freezes OpenType features in fonts.
+"""
+
+def freeze_font(input_font_path, output_font_path):
+    # Logic to freeze OpenType features in fonts
+    pass
+
+if __name__ == '__main__':
+    input_font = 'input_font.otf'  # Placeholder for input font path
+    output_font = 'output_font.otf'  # Placeholder for output font path
+    freeze_font(input_font, output_font)
